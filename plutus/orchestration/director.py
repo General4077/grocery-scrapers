0926592adc6e707e8f9920f849e7194a3c30c1e8
Jayson Scruggs <jayson.scruggs@gmail.com>
2024-02-05 06:25:05 -0600

    remove dead code

--- plutus/orchestration/director.py
+++ plutus/orchestration/director.py
@@ -42,23 +42,6 @@ class Director:
                     url=todo["url"], html=scraper.page_data
                 )
                 result = scraper.to_result()
-                # offer = scraper.offers
-                # availability = offer.get("availability", None)
-                # if availability is not None:
-                #     availability = str(availability)
-                # pickup = offer.get("pickup", None)
-                # if pickup is not None:
-                #     pickup = str(pickup)
-                # result = Result(
-                #     ip=get_ip(),
-                #     url=todo["url"],
-                #     link_id=todo["link_id"],
-                #     price=offer.get("price", None),
-                #     sale_price=None,
-                #     availability=availability,
-                #     pickup=pickup,
-                #     source=scraper.small_soup,
-                #     status_code=scraper.status_code,
                 result.update(
                     **spider.links(),
                 )
