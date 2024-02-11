15f3b52650a631acfe8b84cda82be6f0d8cda054
Jayson Scruggs <jayson.scruggs@gmail.com>
2024-02-11 04:22:48 -0600

    begin work on state

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 import sqlalchemy as sa
 import streamlit as st
 
@@ -7,19 +8,24 @@ engine = sa.create_engine(os.environ.get("PLUTUS_DB_URI"))
 cnx = st.connection("plutus_db", type="sql")
 
 
-@st.cache_data(ttl=30)
-def paint_dataframe():
-    df = cnx.query(
-        "SELECT active, type, url, updated_at, created_at, id FROM plutus_links"
-    )
-    st.data_editor(df, hide_index=True)
+def update_table():
+    state = st.session_state["links_df"]
+    if state.get("edited_rows"):
+        idx = pd.Index(list(state["edited_rows"].keys()))
+        print(idx)
+    if state.get("added_rows"):
+        idx = pd.Index(list(state["added_rows"].keys()))
+        print(idx)
+    if state.get("deleted_rows"):
+        idx = pd.Index(list(state["deleted_rows"].keys()))
+        print(idx)
+
+    print(state)
 
 
-sb_form = st.sidebar.form("options_form")
-url = sb_form.text_input("URL")
-link_type = sb_form.selectbox("Type", ["PRODUCT", "SPIDER"])
-submit = sb_form.form_submit_button("Add Link")
-if submit:
+def insert_link(url: str, link_type: str, strip_params: bool = True) -> bool:
+    if strip_params:
+        url = url.split("?")[0]
     with engine.connect() as conn:
         try:
             conn.execute(
@@ -29,7 +35,32 @@ if submit:
                 {"url": url, "type": link_type},
             )
             conn.commit()
+            return True
         except sa.exc.IntegrityError:
             st.warning("Duplicate Value!")
-        paint_dataframe()
+            return False
+
+
+def paint_dataframe():
+    df = cnx.query(
+        "SELECT active, type, url, updated_at, created_at, id FROM plutus_links"
+    )
+    st.data_editor(
+        df,
+        hide_index=True,
+        use_container_width=True,
+        on_change=update_table,
+        num_rows="dynamic",
+        key="links_df",
+    )
+
+
+sb_form = st.sidebar.form("options_form")
+url = sb_form.text_input("URL")
+link_type = sb_form.selectbox("Type", ["PRODUCT", "SPIDER"])
+submit = sb_form.form_submit_button("Add Link")
+strip_params = sb_form.checkbox("Strip URL Params", value=True)
+if submit:
+    print(insert_link(url, link_type, strip_params))
+    st.rerun()
 paint_dataframe()
