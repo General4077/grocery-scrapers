a7dc9b829d045f90ecf872298c25c9848aa427e8
Jayson Scruggs <jayson.scruggs@gmail.com>
2024-02-17 11:17:06 -0600

    ...

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -4,26 +4,56 @@ import pandas as pd
 import sqlalchemy as sa
 import streamlit as st
 
-engine = sa.create_engine(os.environ.get("PLUTUS_DB_URI"))
+from plutus.orchestration.models.links import Link
+
+engine = sa.create_engine(
+    os.environ.get("PLUTUS_DB_URI", "")
+)  # TODO: use st.secrets instead of os.environ.get("PLUTUS_DB_URI")
 cnx = st.connection("plutus_db", type="sql")
 
 
-def update_table():
+def update_table(df):
     state = st.session_state["links_df"]
-    if state.get("edited_rows"):
-        idx = pd.Index(list(state["edited_rows"].keys()))
-        print(idx)
-    if state.get("added_rows"):
-        idx = pd.Index(list(state["added_rows"].keys()))
-        print(idx)
-    if state.get("deleted_rows"):
-        idx = pd.Index(list(state["deleted_rows"].keys()))
-        print(idx)
+    with engine.begin() as cnx:
+        if state.get("edited_rows"):
+            idx = pd.Index(list(state["edited_rows"].keys()))
 
-    print(state)
+            # TODO use the bulk update syntax(https://docs.sqlalchemy.org/en/20/tutorial/data_update.html) with to_json() for fast updates
+
+            df.iloc[idx].apply(
+                lambda x: cnx.execute(
+                    sa.update(Link)
+                    .where(Link.id == x["id"])
+                    .values(active=x["active"], type=x["type"], url=x["url"])
+                    .returning(Link.id),
+                ),
+                axis=1,
+            )
+            cnx.commit()
+        if state.get("added_rows"):
+            idx = pd.Index(list(state["added_rows"].keys()))
+            print(idx)
+        if state.get("deleted_rows"):
+            idx = pd.Index(list(state["deleted_rows"].keys()))
+            print(idx)
+    del st.session_state["links_df"]
 
 
-def insert_link(url: str, link_type: str, strip_params: bool = True) -> bool:
+df = cnx.query("SELECT active, type, url, updated_at, created_at, id FROM plutus_links")
+st.data_editor(
+    df,
+    hide_index=True,
+    use_container_width=True,
+    on_change=update_table,
+    num_rows="dynamic",
+    key="links_df",
+    args=(df,),
+)
+
+
+def insert_link(
+    url: str, link_type: str = "PRODUCT", strip_params: bool = True
+) -> bool:
     if strip_params:
         url = url.split("?")[0]
     with engine.connect() as conn:
@@ -41,21 +71,7 @@ def insert_link(url: str, link_type: str, strip_params: bool = True) -> bool:
             return False
 
 
-def paint_dataframe():
-    df = cnx.query(
-        "SELECT active, type, url, updated_at, created_at, id FROM plutus_links"
-    )
-    st.data_editor(
-        df,
-        hide_index=True,
-        use_container_width=True,
-        on_change=update_table,
-        num_rows="dynamic",
-        key="links_df",
-    )
-
-
-sb_form = st.sidebar.form("options_form")
+sb_form = st.sidebar.form(key="options_form")
 url = sb_form.text_input("URL")
 link_type = sb_form.selectbox("Type", ["PRODUCT", "SPIDER"])
 submit = sb_form.form_submit_button("Add Link")
@@ -63,4 +79,3 @@ strip_params = sb_form.checkbox("Strip URL Params", value=True)
 if submit:
     print(insert_link(url, link_type, strip_params))
     st.rerun()
-paint_dataframe()
